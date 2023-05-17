2184289f42a3a1e65ed71bb5baf2c7cb881129c3
aleksandrmakarov-dev <ac5295@student.jamk.fi>
2023-05-18 01:08:21 +0300

    exercises 3.1-3.6 done

--- part-3/phonebook-backend/index.ts
+++ part-3/phonebook-backend/index.ts
@@ -6,7 +6,7 @@ interface Person{
     number:string
 }
 
-const persons:Person[] = [
+let persons:Person[] = [
     { 
         id: 1,
         name: "Arto Hellas", 
@@ -33,6 +33,10 @@ const app:Express = express();
 
 app.use(express.json());
 
+const getPersonById = (id:number):Person | undefined =>{
+    return persons.find((person:Person)=>person.id === id);
+}
+
 app.get('/', (req:Request, res:Response) =>{
     res.json({message:'hello world!'});
 })
@@ -46,6 +50,55 @@ app.get('/api/persons', (req:Request, res:Response) =>{
 app.get('/api/info', (req:Request, res:Response) =>{
     const date = Date();
     res.end(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`);
+});
+
+//GET person by id
+app.get('/api/persons/:id', (req:Request, res:Response) =>{
+    const id = Number(req.params.id);
+    const person = getPersonById(id);
+    if(!person){
+        res.status(404).json({error:`person wit id = ${id} not found`});
+    }else{
+        res.json(person);
+    }
+})
+
+//DELETE person by id
+app.delete('/api/persons/:id', (req:Request, res:Response) =>{
+    const id = Number(req.params.id);
+    const person = getPersonById(id);
+    if(!person){
+        res.status(404).json({error:`person wit id = ${id} not found`});
+    }else{
+        persons = persons.filter((person:Person) =>person.id !== id);
+        res.status(204).end();
+    }
+});
+
+//POST person
+app.post('/api/persons', (req:Request, res:Response) =>{
+    const body:Person = req.body;
+    if(!body.name){
+        return res.status(400).json({error:'name is missing'});
+    }
+
+    if(!body.number){
+        return res.status(400).json({error:'number is missing'});
+    }
+
+    const existingPerson = persons.find((person:Person) =>person.name.toLocaleLowerCase() === body.name.toLocaleLowerCase());
+    if(existingPerson !== undefined){
+        return res.status(400).json({error:`person with name ${body.name} already exists`});
+    }
+
+    const newPerson:Person = {
+        id:Math.round(Math.random()*1000000),
+        name:body.name,
+        number:body.number
+    }
+
+    persons = persons.concat(newPerson);
+    return res.json(newPerson);
 });
 
 
