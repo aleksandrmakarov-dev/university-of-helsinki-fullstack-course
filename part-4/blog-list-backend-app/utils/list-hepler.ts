5a547dc634d25412c72e1f9656ec70eec156444e
aleksandrmakarov-dev <ac5295@student.jamk.fi>
2023-05-26 15:11:06 +0300

    exercises 4.4 - 4.7 done

--- part-4/blog-list-backend-app/utils/list-hepler.ts
+++ part-4/blog-list-backend-app/utils/list-hepler.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { Blog } from '../models/blog';
 
 const dummy = (_blogs: Blog[]) => {
@@ -9,7 +10,41 @@ const totalLikes = (blogs: Blog[]) => {
   return sum;
 };
 
+const favoriteBlog = (blogs: Blog[]) => {
+  const maxLikes: number = Math.max(...blogs.map((blog: Blog) => blog.likes));
+  return blogs.find((blog: Blog) => blog.likes === maxLikes);
+};
+
+const mostBlogs = (blogs: Blog[]) => {
+  const mostBlogsAuthor = _.chain(blogs)
+    .groupBy((blog: Blog) => blog.author)
+    .map((associatedBlogs: Blog[], author: string) => ({
+      author,
+      blogs: associatedBlogs.length,
+    }))
+    .maxBy(obj => obj.blogs)
+    .value();
+
+  return mostBlogsAuthor;
+};
+
+const mostLikes = (blogs: Blog[]) => {
+  const mostLikesAuthor = _.chain(blogs)
+    .groupBy((blog: Blog) => blog.author)
+    .map((associatedBlogs: Blog[], author: string) => ({
+      author,
+      likes: associatedBlogs.reduce((accumulator: number, blog: Blog) => accumulator + blog.likes, 0),
+    }))
+    .maxBy(obj => obj.likes)
+    .value();
+
+  return mostLikesAuthor;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
