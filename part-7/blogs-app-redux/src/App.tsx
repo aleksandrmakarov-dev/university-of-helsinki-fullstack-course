a94793fccfff115402f3cce6c2205d924556b476
aleksandrmakarov-dev <ac5295@student.jamk.fi>
2023-07-30 19:12:14 +0300

    task 7.9 done

---
+++ part-7/blogs-app-redux/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Container } from '@mui/material';
+
+const App = () => {
+  return (
+    <Container>
+      <div>Hello!</div>
+    </Container>
+  );
+};
+
+export default App;
